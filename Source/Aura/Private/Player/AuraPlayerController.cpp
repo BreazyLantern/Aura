80353c50820c2b73c05ba2d19b88f7f308f9df59
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-12-02 18:50:01 -0500

    startup content

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Druid Mechanics
+
+
+#include "Player/AuraPlayerController.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+}
