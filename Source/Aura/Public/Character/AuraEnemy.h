0d05bbdfd6beaa6f8a15ff4fcda5f6ad1a4368f6
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-01-05 01:04:38 -0500

    Detect overlapping actors within the volume of the attack being made by the melee attack.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -28,13 +28,13 @@ public:
 	/** Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
+	virtual AActor* GetCombatTarget_Implementation() const override;
 	/** end Enemy Interface */
 
 	/** Combat Interface*/
 	virtual int32 GetPlayerLevel() override;
 	virtual void Die() override;
-	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
-	virtual AActor* GetCombatTarget_Implementation() const override;
 	/** end Combat Interface */
 
 	UPROPERTY(BlueprintAssignable)
