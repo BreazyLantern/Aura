ae15e2eca33fa970061de430ee55b08cf372d3c9
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-01-06 04:03:44 -0500

    Begin creation of the summoning ability

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,37 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Summoning")
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	int32 NumMinionsToSpawn = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 250.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnSpread = 90.f;
+
+	UPROPERTY(EditAnywhere, Category = "Summoning|Debug")
+	bool DebugLines = false;
+};
