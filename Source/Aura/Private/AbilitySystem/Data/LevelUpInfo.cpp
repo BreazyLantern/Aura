bf127cdac42f9c06d49b2ac4cf5dae46af96c7c4
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-01-07 19:32:41 -0500

    LevelUpInformation DataAsset for level up information on the requirements and rewards per level

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,29 @@
+// Copyright Druid Mechanics
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// Note this could be done using data table curves and such however we are setting them manually for the rewards and requirements per level.
+		// LevelUpInformation[0] is placeholder
+		// LevelUpInformation[1] = Level 1 Information
+		// LevelUpInformation[2] = Level 1 Information
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+
+	return Level;
+}
